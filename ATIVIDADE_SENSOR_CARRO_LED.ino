dcb8e9f61004ec2fe459bfd61850402f3827eed5
Victor <victorsbbv_dev@hotmail.com>
2024-09-16 21:44:46 -0300

    - Added Atividade_sensor_carro_led

---
+++ ATIVIDADE_SENSOR_CARRO_LED.ino
@@ -0,0 +1,77 @@
+int buzzer = 2;
+
+int inches = 0;
+
+int cm = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  pinMode(8, OUTPUT); // VERDE
+  pinMode(9, OUTPUT); // AMARELO
+  pinMode(10, OUTPUT); // VERMELHO
+  pinMode(buzzer, OUTPUT); // BUZZER
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  // measure the ping time in cm
+  cm = 0.01723 * readUltrasonicDistance(5, 7);
+  // convert to inches by dividing by 2.54
+  inches = (cm / 2.54);
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.println("cm");
+  delay(100); // Wait for 100 millisecond(s)
+  
+  if(cm <= 10){
+    noTone(buzzer);
+    tone(buzzer, 300, 100);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+  }
+  else{
+  if(cm > 10 && cm <=20){
+    noTone(buzzer);
+    tone(buzzer, 300, 100);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(8, LOW);
+    delay(200);
+  }
+  else{
+  if(cm > 20 && cm <=30){
+    noTone(buzzer);
+    tone(buzzer, 300, 100);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    delay(500);
+  }
+  else{
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+  }
+  }
+  }
+  
+  
+}
